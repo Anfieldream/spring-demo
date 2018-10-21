d86b55c2878e14183355a902d7c13d1759a94dff
eric <moon_elf@163.com>
2018-10-21 22:57:23 +0800

    update

---
+++ src/main/java/com/demo/springdemo/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.demo.springdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String helloWorld(){
+        return "hello world.";
+    }
+}
